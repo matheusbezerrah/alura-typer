f9e82f959304c9cc44aaf29c371999facfb8c6ef
unknown <Matheus Bezerra>
2018-03-06 10:27:17 -0300

    Alteração 1

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,24 @@
-var frase = $("#frase").text();
-var numPalavras = frase.split(" ").length;
+/*
+** retorna a quantidade de palavras de um conteúdo
+*/
+function contaPalavras(cont){
+	return cont.split(/\S+/).length -1;
+}
+
+var frase = $(".frase").text();
+
+//var numPalavras = frase.split(/\S+/).length -1;
+var numPalavras = contaPalavras(frase);
 $("#qtdPalavras").text(numPalavras);
+
+var campo = $(".campo-digitacao");
+
+$(".campo-digitacao").on("input",function(){
+	var conteudo = $(".campo-digitacao").val();
+	
+	var qtdPalavrasArea = contaPalavras(conteudo);
+	$("#qtdPalavrasTxtArea").text(qtdPalavrasArea);
+
+	var qtdCaracteresArea = conteudo.length;
+	$("#qtdCaracteresTxtArea").text(qtdCaracteresArea);;
+});
